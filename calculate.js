b27efe581d86e25ecc954e1bd3ad37210a02d52f
noo-one <163518933+noo-one@users.noreply.github.com>
2024-04-04 13:52:29 +0200

    Update calculate.js

--- calculate.js
+++ calculate.js
@@ -1,1 +1,41 @@
-const result = document.querySelector("#one");
+const resultElement = document.querySelector(".risulte");
+let result = 0;
+
+// Update the result element with the content of the result variable
+
+resultElement.textContent = result;
+
+// Add an event listener to each button
+const allButtons = document.querySelectorAll(".button-number");
+
+for (let i = 0; i < allButtons.length; i++) {
+  const button = allButtons[i];
+  button.addEventListener("click", function () {
+    result += button.textContent;
+    resultElement.textContent = result;
+  });
+}
+
+document.querySelector("#reset").addEventListener("click", function () {
+  result = 0;
+  resultElement.textContent = result;
+});
+
+document.querySelector("#del").addEventListener("click", function () {
+  result = result.slice(0, -1);
+  resultElement.textContent = result;
+});
+
+const switchToggle = document.querySelector(".switch > input");
+
+switchToggle.addEventListener("change", function () {
+  if (switchToggle.checked === true) {
+    document.body.style.backgroundColor = "red";
+    document.querySelector(".calculate").style.backgroundColor = "yellow";
+    resultElement.style.backgroundColor = "blue";
+  } else {
+    document.body.style.backgroundColor = "grey";
+    document.querySelector(".calculate").style.backgroundColor = "orange";
+    resultElement.style.backgroundColor = "pink";
+  }
+});
